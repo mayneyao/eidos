0732f45349cde6d2e75fb95334cc0f2342b06991
Mayne <i@gine.me>
2023-06-20 23:45:45 +0800

    fix: number field icon

--- components/grid/fields/header-icons.ts
+++ components/grid/fields/header-icons.ts
@@ -5,7 +5,8 @@ import { FieldType } from "@/lib/fields/const"
 export const headerIcons: SpriteMap = {
   [FieldType.Text]: (p) =>
     `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-baseline"><path d="M4 20h16"/><path d="m6 16 6-12 6 12"/><path d="M8 12h8"/></svg>`,
-  [FieldType.Number]: (p) => "🔢",
+  [FieldType.Number]: (p) =>
+    `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hash"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>`,
   [FieldType.Checkbox]: (p) =>
     `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-square"><polyline points="9 11 12 14 22 4"/><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/></svg>`,
   [FieldType.Date]: (p) =>
