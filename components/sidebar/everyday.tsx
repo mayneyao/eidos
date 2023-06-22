20f506427cb1c46efdb19e68f4ebdce068c1c94a
Mayne <i@gine.me>
2023-06-23 01:21:18 +0800

    feat: improve logseq import

--- components/sidebar/everyday.tsx
+++ components/sidebar/everyday.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Link from "next/link"
 import { CalendarDays } from "lucide-react"
 
@@ -19,31 +20,49 @@ import {
 } from "@/components/ui/dialog"
 
 import { Button } from "../ui/button"
+import { Progress } from "../ui/progress"
 
 // convertMarkdown2State
 export const EverydaySidebarItem = ({ space }: { space: string }) => {
   const { sqlite } = useSqlite(space)
   const { convertMarkdown2State } = useDocEditor(sqlite)
+  const [progress, setProgress] = useState(0)
+  const [importing, setImporting] = useState(false)
+
   const getDir = async () => {
     if (!sqlite) return
     const dirHandle = await (window as any).showDirectoryPicker()
     const oldDays = await sqlite.listDays()
     const oldDayFileNameSet = new Set(oldDays.map((d) => d.name))
+    const allDays = []
     for await (let [name, handle] of dirHandle.entries()) {
+      allDays.push(name)
+    }
+    const allCount = allDays.length
+    setImporting(true)
+    let index = 0
+    for await (let [name, handle] of dirHandle.entries()) {
+      const _p = (index / allCount) * 100
+      setProgress(_p)
+      index++
       if (oldDayFileNameSet.has(name)) {
+        console.log(index, name, "skip")
         continue
       } else if (name.endsWith(".md")) {
+        console.log(index, _p)
         const fileHandle = handle as FileSystemFileHandle
         const file = await fileHandle.getFile()
         const content = await file.text()
         try {
+          const newFilename = name.split("_").join("-")
           const state = await convertMarkdown2State(content)
-          await sqlite.createDayNote(name, state)
+          await sqlite.createDayNote(newFilename, state)
         } catch (error) {
           console.warn(error)
         }
       }
     }
+    setImporting(false)
   }
 
   return (
@@ -63,8 +82,8 @@ export const EverydaySidebarItem = ({ space }: { space: string }) => {
           </Button>
         </ContextMenuTrigger>
         <ContextMenuContent>
-          <ContextMenuItem>Open</ContextMenuItem>
-          <ContextMenuItem>Download</ContextMenuItem>
+          <ContextMenuItem disabled>Open</ContextMenuItem>
+          <ContextMenuItem disabled>Download</ContextMenuItem>
           <DialogTrigger asChild>
             <ContextMenuItem>
               <span>Import</span>
@@ -74,13 +93,13 @@ export const EverydaySidebarItem = ({ space }: { space: string }) => {
       </ContextMenu>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Import</DialogTitle>
+          <DialogTitle>Import From Logseq(Beta)</DialogTitle>
         </DialogHeader>
-        <div>
-          <div onClick={getDir}>select logseq file </div>
+        <div className="flex h-20 cursor-pointer items-center justify-center border border-dashed border-slate-500 p-2">
+          <div onClick={getDir}>click here to select logseq journal folder</div>
         </div>
         <DialogFooter>
-          <Button type="submit">Import</Button>
+          {importing && <Progress value={progress} />}
         </DialogFooter>
       </DialogContent>
     </Dialog>
