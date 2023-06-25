b4a8cab4d304962ca238d7199f520369d0d23397
Mayne <i@gine.me>
2023-06-26 01:37:29 +0800

    fix: api

--- app/api/openapi.json/route.ts
+++ app/api/openapi.json/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server"
-import { appRouter, getOpenApiDocument } from "@/api/router"
-import { generateOpenApiDocument } from "trpc-openapi"
 
 export async function GET() {
-  const openApiDocument = generateOpenApiDocument(appRouter, {
-    title: "tRPC OpenAPI",
-    version: "1.0.0",
-    baseUrl: "http://localhost:3001",
-    docsUrl: "/docs",
-  })
-
-  return NextResponse.json(openApiDocument)
+  // const openApiDocument = generateOpenApiDocument(appRouter, {
+  //   title: "tRPC OpenAPI",
+  //   version: "1.0.0",
+  //   baseUrl: "http://localhost:3001",
+  //   docsUrl: "/docs",
+  // })
+  return NextResponse.json({ msg: "not available" })
 }
