183bd6b3a94924dcfcf0002551c15ab0eb77ae9b
Mayne <i@gine.me>
2023-07-20 03:44:46 +0800

    fix: build

--- worker/meta_table/file.ts
+++ worker/meta_table/file.ts
@@ -1,8 +1,8 @@
-import { FileTableName } from "@/lib/sqlite/const"
+// import { FileTableName } from "@/lib/sqlite/const"
 
-import { BaseTable } from "./base"
+// import { BaseTable } from "./base"
 
-export class FileTable implements BaseTable {
-  name = FileTableName
-  createTableSql = ``
-}
+// export class FileTable implements BaseTable {
+//   name = FileTableName
+//   createTableSql = ``
+// }
--- worker/meta_table/todo.ts
+++ worker/meta_table/todo.ts
@@ -1,15 +1,15 @@
-import { TodoTableName } from "@/lib/sqlite/const"
+// import { TodoTableName } from "@/lib/sqlite/const"
 
-import { BaseTable } from "./base"
+// import { BaseTable } from "./base"
 
-export class FileTable implements BaseTable {
-  name = TodoTableName
-  createTableSql = `
-  CREATE TABLE IF NOT EXISTS ${TodoTableName} (
-    content TEXT,
-    done BOOLEAN,
-    doc_id TEXT,
-    list_id TEXT,
-    node_key TEXT
-  );`
-}
+// export class FileTable implements BaseTable {
+//   name = TodoTableName
+//   createTableSql = `
+//   CREATE TABLE IF NOT EXISTS ${TodoTableName} (
+//     content TEXT,
+//     done BOOLEAN,
+//     doc_id TEXT,
+//     list_id TEXT,
+//     node_key TEXT
+//   );`
+// }
