a788b0f6d9742c6ba25876073cd320891142f4d4
Mayne <i@gine.me>
2023-08-05 18:48:27 +0800

    feat(space): remove space

--- app/space-manage/page.tsx
+++ app/space-manage/page.tsx
@@ -1,24 +1,56 @@
 "use client"
 
+import {
+  Download,
+  MoreHorizontalIcon,
+  Trash2Icon,
+  UploadCloud,
+} from "lucide-react"
 import { useState } from "react"
-import { Download, UploadCloud } from "lucide-react"
 import { Link } from "react-router-dom"
 
-import { exportSpace, importSpace } from "@/lib/space"
-import { useSpace } from "@/hooks/use-space"
-import { useSync } from "@/hooks/use-sync"
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { toast } from "@/components/ui/use-toast"
+import { useSpace } from "@/hooks/use-space"
+import { useSync } from "@/hooks/use-sync"
+import { exportSpace, importSpace, removeSpace } from "@/lib/space"
 
+type IActionMode = "remove" | "export" | "upload"
 export default function SpaceManagerPage() {
   const { spaceList, updateSpaceList } = useSpace()
   const [loading, setLoading] = useState(false)
   const [file, setFile] = useState(null)
   const [spaceName, setSpaceName] = useState("")
+  const [mode, setMode] = useState<IActionMode>()
+  const [modeLoading, setModeLoading] = useState(false)
   const { push } = useSync()
   const handleFileChange = (e: any) => {
     e.target.files[0] && setFile(e.target.files[0])
+  }
+
+  const handleAction = async (space: string, mode: IActionMode) => {
+    setModeLoading(true)
+    setMode(mode)
+    switch (mode) {
+      case "remove":
+        await removeSpace(space)
+        break
+      case "upload":
+        await push(space)
+        break
+      case "export":
+        await exportSpace(space)
+        break
+    }
+    updateSpaceList()
+    setModeLoading(false)
   }
 
   const handleImport = async () => {
@@ -41,7 +73,6 @@ export default function SpaceManagerPage() {
   return (
     <div className="prose mx-auto flex flex-col gap-2 p-10 dark:prose-invert">
       Import Space from file
-      {loading && <div>importing...</div>}
       <div className="mt-2 flex items-center gap-2">
         <Input
           type="text"
@@ -53,6 +84,8 @@ export default function SpaceManagerPage() {
           Import{" "}
         </Button>
       </div>
+      {loading && <div>importing...</div>}
+      {modeLoading && <div>{mode} ...</div>}
       <hr />
       {spaceList.map((space) => {
         return (
@@ -63,12 +96,31 @@ export default function SpaceManagerPage() {
             <div className="grow">
               <Link to={`/${space}`}>{space}</Link>
             </div>
-            <Button variant="ghost" onClick={() => push(space)}>
-              <UploadCloud className="h-4 w-4" />
-            </Button>
-            <Button variant="ghost" onClick={() => exportSpace(space)}>
-              <Download className="h-4 w-4" />
-            </Button>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="ghost">
+                  <MoreHorizontalIcon className="h-5 w-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  onSelect={() => handleAction(space, "export")}
+                >
+                  <Download className="mr-2 h-4 w-4" /> <span>Export</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={() => handleAction(space, "upload")}
+                >
+                  <UploadCloud className="mr-2 h-4 w-4" /> <span>Upload</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={() => handleAction(space, "remove")}
+                >
+                  <Trash2Icon className="mr-2 h-4 w-4" /> <span>Remove</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         )
       })}
