9fb65af06c44bfe11cbea08abc00b94e823428c7
Mayne <i@gine.me>
2023-08-12 23:46:06 +0800

    fix: everyday layout

--- app/[database]/everyday/[day]/page.tsx
+++ app/[database]/everyday/[day]/page.tsx
@@ -24,7 +24,7 @@ export default function EverydayPage() {
   }
 
   return (
-    <div className="prose mx-auto p-4 lg:prose-xl xl:prose-2xl">
+    <div className="prose mx-auto w-full p-4 lg:prose-xl xl:prose-2xl">
       <Editor
         isEditable
         onSave={handleSaveDoc}
