6a565208b15230093c08f59ea3732d07077dc297
Mayne <i@gine.me>
2023-10-28 06:25:34 +0800

    feat: build link query

--- lib/sqlite/sql-parser.ts
+++ lib/sqlite/sql-parser.ts
@@ -7,11 +7,45 @@ import {
   toSql,
 } from "pgsql-ast-parser"
 
+import type { IUIColumn } from "@/hooks/use-table"
+
+import { FieldType } from "../fields/const"
+
 export const getColumnsFromQuery = (sql?: string) => {
   if (!sql) return []
   // parse multiple statements
   const ast: Statement[] = parse(sql)
   return (ast?.[0] as SelectFromStatement).columns
+}
+
+/**
+ * 1. every user-created-table has a `_id` and a `title` column
+ * 2. to render a table, first we query data.
+ * 2.1 if the table has link fields, we need to join the link table to get the title
+ * @param uiColumnMap name -> IUIColumn map of the table
+ * @returns
+ */
+export const getLinkQuery = (uiColumnMap: Map<string, IUIColumn>) => {
+  const rawColumns = Array.from(uiColumnMap.values())
+  const thisTable = rawColumns[0].table_name
+  const linkColumns = rawColumns.filter((c) => c.type === FieldType.Link)
+  const queryList: {
+    columnName: string
+    sql: string
+  }[] = []
+
+  for (const column of linkColumns) {
+    const linkTable = column.property.linkTable
+    let sql = "SELECT "
+    sql += `thisTable.${column.table_column_name}`
+    sql += `, linkTable.title as ${column.table_column_name}__title `
+    sql += `FROM ${thisTable} as thisTable INNER JOIN ${linkTable} as linkTable ON thisTable.${column.table_column_name} = linkTable._id`
+    queryList.push({
+      columnName: column.table_column_name,
+      sql,
+    })
+  }
+  return queryList
 }
 
 /**
