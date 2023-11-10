2315ac965a2b27c727578bf2ba08a256573f7783
Mayne <i@gine.me>
2023-11-11 02:19:18 +0800

    fix(tauri): add localhost plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{utils::config::AppUrl, window::WindowBuilder, WindowUrl};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .setup(move |app| {
+            WindowBuilder::new(
+                app,
+                "main".to_string(),
+                if cfg!(dev) {
+                    Default::default()
+                } else {
+                    window_url
+                },
+            )
+            .title("Localhost Example")
+            .build()?;
+            Ok(())
+        })
+        .run(context)
+        .expect("error while running tauri application");
 }
