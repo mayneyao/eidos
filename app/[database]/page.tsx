a00a6bb56a28f823edb2833d14f1f969776b6e1d
Mayne <i@gine.me>
2024-04-10 15:42:01 +0800

    fix: update space home page

--- app/[database]/page.tsx
+++ app/[database]/page.tsx
@@ -1,7 +1,11 @@
+import { EidosIcon } from "@/components/icons/eidos"
+
 export default function DatabaseHome() {
   return (
-    <div className="item-center flex justify-center p-8">
-      select a table to view
+    <div className="item-center flex h-full w-full items-center justify-center p-8">
+      <div className="opacity-40">
+        <EidosIcon />
+      </div>
     </div>
   )
 }
