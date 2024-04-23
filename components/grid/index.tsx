cad379e8c08636604b5497a6be94539d24133fdc
Mayne <i@gine.me>
2024-04-23 18:07:34 +0800

    fix: grid style

--- components/grid/index.tsx
+++ components/grid/index.tsx
@@ -253,7 +253,7 @@ export default function GridView(props: IGridProps) {
                 <Button
                   size="sm"
                   variant="ghost"
-                  className="w-full rounded-none"
+                  className="w-full rounded-none flex justify-start"
                   onClick={() => {
                     setIsAddFieldEditorOpen(true)
                   }}
