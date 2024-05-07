1a933c2ade835dfcf374b9d8ca178528514cd62d
Mayne <i@gine.me>
2024-05-08 02:51:13 +0800

    feat: show LLM download progress

--- app/settings/ai/local-llm-manage.tsx
+++ app/settings/ai/local-llm-manage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-import { AIModelSelect } from "@/components/ai-chat/ai-chat-model-select"
-import { WEB_LLM_MODELS } from "@/components/ai-chat/webllm/models"
+import { downloadWebLLM } from "@/lib/ai/helper"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -11,8 +11,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { downloadWebLLM } from "@/lib/ai/helper"
-import { cn } from "@/lib/utils"
+import { AIModelSelect } from "@/components/ai-chat/ai-chat-model-select"
+import { WEB_LLM_MODELS } from "@/components/ai-chat/webllm/models"
 
 const controller = new AbortController()
 
@@ -51,9 +51,9 @@ export const LocalLLMManage = (props: {
     <div>
       <Card>
         <CardHeader>
-          <CardTitle>Local LLM Models</CardTitle>
+          <CardTitle>Local LLM</CardTitle>
           <CardDescription>
-            <p>Manage your local LLM models.</p>
+            <p>Manage your local LLM.</p>
           </CardDescription>
         </CardHeader>
         <CardFooter className="flex gap-2">
@@ -87,14 +87,19 @@ export const LocalLLMManage = (props: {
                 )}
               >
                 {Boolean(currentModel && progress != 1) && (
-                  <div
-                    className={cn(
-                      "pointer-events-none absolute left-0 top-0 h-full bg-cyan-500 opacity-25 transition-all"
-                    )}
-                    style={{
-                      width: `${progress * 100}%`,
-                    }}
-                  />
+                  <>
+                    <div
+                      className={cn(
+                        "pointer-events-none absolute left-0 top-0 h-full bg-cyan-500 opacity-25 transition-all"
+                      )}
+                      style={{
+                        width: `${progress * 100}%`,
+                      }}
+                    />
+                    <span className="pointer-events-none absolute right-0 top-0 flex h-full items-center pr-2 ">
+                      {(progress * 100).toFixed(2)}%
+                    </span>
+                  </>
                 )}
                 {currentModel}
               </li>
