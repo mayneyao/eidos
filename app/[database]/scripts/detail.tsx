06cb4c052cabe9454eabccaa0bdafd3c5351b769
Mayne <i@gine.me>
2024-05-13 16:50:55 +0800

    fix(extension):  script update logic

--- app/[database]/scripts/detail.tsx
+++ app/[database]/scripts/detail.tsx
@@ -1,15 +1,15 @@
+import { Suspense, lazy, useCallback, useEffect, useState } from "react"
 import { IScript } from "@/worker/web-worker/meta-table/script"
 import { useMount } from "ahooks"
-import { Suspense, lazy, useCallback, useEffect, useState } from "react"
 import { useLoaderData, useNavigate, useRevalidator } from "react-router-dom"
 
+import { useCurrentPathInfo } from "@/hooks/use-current-pathinfo"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Switch } from "@/components/ui/switch"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useToast } from "@/components/ui/use-toast"
-import { useCurrentPathInfo } from "@/hooks/use-current-pathinfo"
 
 import { ExtensionConfig } from "./config/config"
 // import { CodeEditor } from "./editor/code-editor"
@@ -34,24 +34,22 @@ export const ScriptDetailPage = () => {
     revalidator.revalidate()
   })
 
-  const { ref: nameRef } = useEditableElement({
-    onSave(value) {
+  const handleSave = useCallback(
+    (value: any, key: string) => {
       updateScript({
         ...script,
-        name: value,
+        [key]: value,
       })
       revalidator.revalidate()
     },
+    [revalidator, script, updateScript]
+  )
+  const { ref: nameRef } = useEditableElement({
+    onSave: (value) => handleSave(value, "name"),
   })
 
   const { ref: descRef } = useEditableElement({
-    onSave(value) {
-      updateScript({
-        ...script,
-        description: value,
-      })
-      revalidator.revalidate()
-    },
+    onSave: (value) => handleSave(value, "description"),
   })
 
   const { toast } = useToast()
@@ -86,32 +84,53 @@ export const ScriptDetailPage = () => {
   }
 
   return (
-    <div className="p-6">
-      <Tabs defaultValue="account" className="w-[600px] md:w-[920px]">
-        <TabsList>
-          <TabsTrigger value="account">Basic</TabsTrigger>
-          <TabsTrigger value="password">Settings</TabsTrigger>
-        </TabsList>
+    <Tabs
+      defaultValue="account"
+      className="flex h-full w-full flex-col overflow-hidden p-6"
+    >
+      <TabsList className=" w-max">
+        <TabsTrigger value="account">Basic</TabsTrigger>
+        <TabsTrigger value="password">Settings</TabsTrigger>
+      </TabsList>
 
-        {revalidator.state === "loading" ? (
-          <Skeleton className="mt-8 h-[20px] w-[100px] rounded-full" />
-        ) : (
-          <>
-            <TabsContent value="account">
-              <div className="flex flex-col gap-4">
-                <div className="flex justify-between">
-                  <h2 className="mb-2 text-xl font-semibold">
-                    <span ref={nameRef}>{script.name}</span> ({script.version})
-                  </h2>
+      <hr className="my-2" />
+
+      {revalidator.state === "loading" ? (
+        <Skeleton className="mt-8 h-[20px] w-[100px] rounded-full" />
+      ) : (
+        <>
+          <TabsContent value="account" className="h-full w-full">
+            <div className="flex h-full flex-col gap-4">
+              <div className="flex justify-between">
+                <h2 className="mb-2 flex items-end  gap-2 text-xl font-semibold">
+                  <span ref={nameRef}>{script.name}</span> ({script.version})
                   <Switch
                     checked={script.enabled}
                     onCheckedChange={(checked) =>
                       handleToggleEnabled(script.id, checked)
                     }
                   ></Switch>
+                </h2>
+                <div className="flex items-center gap-2">
+                  <Button
+                    variant="ghost"
+                    onClick={handleDeleteScript}
+                    size="sm"
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    type="submit"
+                    onClick={() => onSubmit(code)}
+                    size="sm"
+                  >
+                    Update
+                  </Button>
                 </div>
-                <p ref={descRef}>{script.description}</p>
-                <Separator />
+              </div>
+              <p ref={descRef}>{script.description}</p>
+              <Separator />
+              <div className="mb-2 flex grow flex-col">
                 <Suspense
                   fallback={
                     <Skeleton className="h-[20px] w-[100px] rounded-full" />
@@ -126,22 +145,14 @@ export const ScriptDetailPage = () => {
                     }
                   />
                 </Suspense>
-                <div className="flex justify-between">
-                  <Button type="submit" onClick={() => onSubmit(code)}>
-                    Update
-                  </Button>
-                  <Button variant="outline" onClick={handleDeleteScript}>
-                    Delete
-                  </Button>
-                </div>
               </div>
-            </TabsContent>
-            <TabsContent value="password">
-              <ExtensionConfig />
-            </TabsContent>
-          </>
-        )}
-      </Tabs>
-    </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="password">
+            <ExtensionConfig />
+          </TabsContent>
+        </>
+      )}
+    </Tabs>
   )
 }
