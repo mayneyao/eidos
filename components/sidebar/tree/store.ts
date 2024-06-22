6c5d0b9f6ab00e645f18ac7bf5efb33441de6989
datduyng <datnguyen5653@gmail.com>
2024-06-22 11:12:52 -0700

    persist folder toggle state

--- components/sidebar/tree/store.ts
+++ components/sidebar/tree/store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand"
 
 import { ITreeNode } from "@/lib/store/ITreeNode"
+import { persist } from "zustand/middleware"
 
 export interface IHoverTarget extends ITreeNode {
   index: number
@@ -9,24 +10,25 @@ export interface IHoverTarget extends ITreeNode {
 }
 // Define the state shape
 interface FolderState {
-  folders: Record<string, boolean>
-  toggleFolder: (id: string) => void
-  closeFolder: (id: string) => void
   currentCut: string | null
   setCut: (id: string | null) => void
   targetFolderId: string | null
   setTargetFolderId: (id: string | null) => void
-
-  // const [target, setTarget] = useState<{
-  //   index: number
-  //   direction: "up" | "down"
-  // } | null>(null)
   target: IHoverTarget | null
   setTarget: (target: IHoverTarget | null) => void
 }
 
+export const useFolderStore = () => {
+  const folderActionStore = useFolderActionStore()
+  const folderPersistStore = usePersistFolderStore()
+  return {
+    ...folderActionStore,
+    ...folderPersistStore,
+  }
+}
+
 // Create the Zustand store
-export const useFolderStore = create<FolderState>((set) => ({
+export const useFolderActionStore = create<FolderState>((set) => ({
   targetFolderId: null,
   target: null,
   setTarget: (target) =>
@@ -45,19 +47,32 @@ export const useFolderStore = create<FolderState>((set) => ({
     set(() => ({
       currentCut: id,
     })),
-  folders: {},
-  toggleFolder: (id: string) =>
-    set((state) => ({
-      folders: {
-        ...state.folders,
-        [id]: !state.folders[id],
-      },
-    })),
-  closeFolder: (id: string) =>
-    set((state) => ({
-      folders: {
-        ...state.folders,
-        [id]: false,
-      },
-    })),
 }))
+
+export const usePersistFolderStore = create<{
+  folders: Record<string, boolean>
+  toggleFolder: (id: string) => void
+  closeFolder: (id: string) => void
+}>()(persist(
+  (set) => ({
+
+    folders: {},
+    toggleFolder: (id: string) =>
+      set((state) => ({
+        folders: {
+          ...state.folders,
+          [id]: !state.folders[id],
+        },
+      })),
+    closeFolder: (id: string) =>
+      set((state) => ({
+        folders: {
+          ...state.folders,
+          [id]: false,
+        },
+      })),
+  }), {
+  name: "folder-persist",
+  getStorage: () => localStorage,
+  }
+))
