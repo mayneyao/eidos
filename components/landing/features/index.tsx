66d0d6522b561e337f2bc60f30fcad9da9368706
mayneyao <i@gine.me>
2024-06-24 05:50:28 +0800

    fix: improve landing page

--- components/landing/features/index.tsx
+++ components/landing/features/index.tsx
@@ -1,4 +1,3 @@
-
 import { FeatureCard, IFeatureCard } from "./card"
 
 const features: IFeatureCard[] = [
@@ -9,24 +8,19 @@ const features: IFeatureCard[] = [
         While Eidos is just a web app with no web server, it also offers an API
         for developers to extend its functionality. Create your own workflows
         with ease.
+        <br />
+        <br />
+        Wait, local web app with an API 🤔? Yes 😎! see{" "}
+        <a
+          href="https://github.com/mayneyao/eidos-api-agent-node"
+          className="text-blue-500 underline"
+        >
+          how it works.
+        </a>
       </p>
     ),
     lightImageUrl: "/show/api-light.webp",
     darkImageUrl: "/show/api-dark.webp",
-  },
-  {
-    title: "Email Integration",
-    description: (
-      <p>
-        Capture ideas, notes, and read/watch-it-later easily.
-        <br />
-        <br />
-        Each resource in Eidos has a unique email address, allowing you to
-        create or update resources simply by sending an email.
-      </p>
-    ),
-    lightImageUrl: "/show/mail-to-eidos.webp",
-    imgCls: "bg-red-100 p-4",
   },
   {
     title: "SQLite Standardization",
@@ -43,10 +37,35 @@ const features: IFeatureCard[] = [
   },
   {
     title: "Offline AI Capabilities",
-    description:
-      "Download LLM once and use it anytime, even without an internet connection. No data leaves your device.",
+    description: (
+      <p>
+        Download LLM once and use it anytime, even without an internet
+        connection. No data leaves your device.
+        <br />
+        <br />
+        It also works well with Ollama and any LLM provider compatible with
+        OpenAI API.
+      </p>
+    ),
     lightImageUrl: "/show/offline-ai.webp",
     imgCls: "bg-teal-100 p-2",
+  },
+]
+
+const officeExtServices: IFeatureCard[] = [
+  {
+    title: "Email Integration",
+    description: (
+      <p>
+        Capture ideas, notes, and read/watch-it-later easily.
+        <br />
+        <br />
+        Each resource in Eidos has a unique email address, allowing you to
+        create or update resources simply by sending an email.
+      </p>
+    ),
+    lightImageUrl: "/show/mail-to-eidos.webp",
+    imgCls: "bg-red-100 p-4",
   },
 ]
 
@@ -107,6 +126,33 @@ export const Features = () => {
       </div>
       <div className="mx-auto grid max-w-sm items-start gap-24 sm:max-w-4xl lg:max-w-5xl">
         {extendFeatures.map((feature, index) => (
+          <FeatureCard key={index} {...feature} even={index % 2 == 0} />
+        ))}
+      </div>
+
+      <hr className="my-8" />
+
+      <div className="container px-4 py-6 xs:px-0 md:px-6">
+        <div className="flex flex-col items-center justify-center py-4 text-center">
+          <div className="space-y-3">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+              Addon Services(optional)
+            </h2>
+
+            <h3 className="text-2xl tracking-tighter sm:text-3xl">
+              Local First. Cloud Opt-in.
+            </h3>
+            <p className="max-w-[900px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              Here are some official services that can enhance your Eidos
+              experience.
+              <br />
+              Still open source! You can build your own services.😎
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto grid max-w-sm items-start gap-24 sm:max-w-4xl lg:max-w-5xl">
+        {officeExtServices.map((feature, index) => (
           <FeatureCard key={index} {...feature} even={index % 2 == 0} />
         ))}
       </div>
