7e019cc8db3a248512ed899a8af01d97d86e2d0b
mayneyao <i@gine.me>
2024-06-28 17:05:06 +0800

    feat: check browser

---
+++ hooks/use-browser-check.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+import * as checker from "@/lib/web/browser"
+
+export const useBrowserCheck = () => {
+  const [isBrowserSupported, setIsBrowserSupported] = useState(false)
+  const [isCoreWebApisSupported, setIsCoreWebApisSupported] = useState(false)
+  const [isOPFSupported, setIsOPFSupported] = useState(false)
+  const [version, setVersion] = useState(0)
+
+  useEffect(() => {
+    setIsBrowserSupported(checker.isBrowserSupported())
+    setIsCoreWebApisSupported(checker.isCoreWebApisSupported())
+    setVersion(checker.getBrowserVersion())
+    checker.isOPFSupported().then((res) => {
+      setIsOPFSupported(res)
+    })
+  }, [isBrowserSupported, isCoreWebApisSupported])
+
+  return { isBrowserSupported, isCoreWebApisSupported, isOPFSupported, version }
+}
