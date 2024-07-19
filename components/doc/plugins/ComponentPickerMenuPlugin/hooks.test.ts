bc498cd13b663bf78ce25ee9838e2f4db8242d72
qinjialei24 <954551222@qq.com>
2024-07-19 13:15:38 +0800

    feat: support Chinese slashes

---
+++ components/doc/plugins/ComponentPickerMenuPlugin/hooks.test.ts
@@ -0,0 +1,75 @@
+import { renderHook } from '@testing-library/react-hooks';
+import {PUNCTUATION, useBasicTypeaheadTriggerMatch} from '@lexical/react/LexicalTypeaheadMenuPlugin';
+import {LexicalEditor} from "lexical";
+
+describe('useBasicTypeaheadTriggerMatch', () => {
+  const editor:LexicalEditor = {} as LexicalEditor;
+
+  it('should match when the trigger is at the start of the text', () => {
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { minLength: 1 })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn('@test',editor);
+    expect(matchResult).toEqual({
+      leadOffset: 0,
+      matchingString: 'test',
+      replaceableString: '@test',
+    });
+  });
+
+  it('should not match when the trigger is followed by punctuation', () => {
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { minLength: 1 })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn('@!',editor);
+    expect(matchResult).toBeNull();
+  });
+
+  it('should match when the trigger is preceded by whitespace', () => {
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { minLength: 1 })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn(' @test',editor);
+    expect(matchResult).toEqual({
+      leadOffset: 1,
+      matchingString: 'test',
+      replaceableString: '@test',
+    });
+  });
+
+  it('should not match when the string length is less than minLength', () => {
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { minLength: 2 })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn('@t',editor);
+    expect(matchResult).toBeNull();
+  });
+
+  it('should match when the string length is equal to maxLength', () => {
+    const maxLength = 10;
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { maxLength })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn('@'.concat('a'.repeat(maxLength)),editor);
+    expect(matchResult).toEqual({
+      leadOffset: 0,
+      matchingString: 'a'.repeat(maxLength),
+      replaceableString: '@'.concat('a'.repeat(maxLength)),
+    });
+  });
+
+  it('should not match when the string length exceeds maxLength', () => {
+    const maxLength = 10;
+    const { result } = renderHook(() =>
+      useBasicTypeaheadTriggerMatch('@', { maxLength })
+    );
+    const triggerFn = result.current;
+    const matchResult = triggerFn('@'.concat('a'.repeat(maxLength + 1)),editor);
+    expect(matchResult).toBeNull();
+  });
+});
