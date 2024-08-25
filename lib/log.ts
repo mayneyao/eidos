b0dc215d600a78f4ad5481c0359c6b31c28c2228
mayneyao <i@gine.me>
2024-08-25 21:13:47 +0800

    Update to version 0.6.3

--- lib/log.ts
+++ lib/log.ts
@@ -1,5 +1,5 @@
 export const logger = console
-export const EIDOS_VERSION = "0.6.2"
+export const EIDOS_VERSION = "0.6.3"
 export const isDevMode = import.meta.env?.MODE === "development"
 export const isSelfHosted = import.meta.env?.VITE_EIDOS_SELF_HOSTED === "true"
 export const isInkServiceMode =
