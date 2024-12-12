ccefbbf0e98de573c98c48ef42a6283b51705851
mayneyao <i@gine.me>
2024-12-12 09:03:38 +0800

    Update to version 0.12.0

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,5 @@
 export const logger = console // TODO: remove this
-export const EIDOS_VERSION = "0.11.0"
+export const EIDOS_VERSION = "0.12.0"
 export const isDevMode = Boolean(import.meta.env?.DEV)
 export const isSelfHosted = import.meta.env?.MODE === "self-host"
 export const isInkServiceMode = import.meta.env?.MODE === "ink"
