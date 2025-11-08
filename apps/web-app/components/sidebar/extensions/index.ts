775ec25f058e4e18f5acd9f2ae6e9b2a65b8e24d
mayneyao <i@gine.me>
2025-11-09 01:36:53 +0800

    refactor(sidebar): remove deprecated extension components and clean up sidebar structure

--- apps/web-app/components/sidebar/extensions/index.ts
+++ apps/web-app/components/sidebar/extensions/index.ts
@@ -1,7 +1,4 @@
-export { ExtensionSidebar } from "./extension-sidebar"
 export { ExtensionSidebarHeader } from "./extension-sidebar-header"
-export { ExtensionList } from "./extension-list"
-export { ExtensionItem } from "./extension-item"
 export { ExtensionSearch } from "./extension-search"
 export { ExtensionSortDropdown } from "./extension-sort-dropdown"
 export { NewExtensionButton } from "./new-extension-button"
