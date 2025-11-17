52fac1db5a1ac4416e5922d3d627cda91176a343
mayneyao <i@gine.me>
2025-11-17 08:40:40 +0800

    fix(sidebar): update cursor styles for sidebar resizing to improve user experience

--- apps/web-app/components/ui/sidebar.tsx
+++ apps/web-app/components/ui/sidebar.tsx
@@ -422,7 +422,7 @@ const SidebarRail = React.forwardRef<
       startWidth.current = width
       setIsResizing(true)
 
-      document.body.style.cursor = "col-resize"
+      document.body.style.cursor = "ew-resize"
       document.body.style.userSelect = "none"
 
       document.addEventListener("mousemove", handleMouseMove)
@@ -462,7 +462,7 @@ const SidebarRail = React.forwardRef<
       }
       className={cn(
         "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] hover:after:bg-sidebar-border group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
-        "[[data-side=left]_&]:cursor-w-resize [[data-side=right]_&]:cursor-e-resize",
+        "[[data-state=expanded]_&]:cursor-ew-resize",
         "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
         "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
