440bcfaeaeccf82d3ecd7eb27d2001bed1e09f9d
Mayne <i@gine.me>
2026-04-08 08:30:46 +0800

    feat: add electron-ipc package for decorator-based IPC communication

---
+++ packages/electron-ipc/src/index.ts
@@ -0,0 +1,21 @@
+// IPC Decorator Framework - Zero Boilerplate IPC Management for Electron
+
+export {
+  IpcService,
+  IpcMethod,
+  IpcServiceBase,
+  createPreloadApi,
+  getIpcMethodNames,
+} from "./decorators"
+
+export type {
+  ExtractIpcApi,
+  IpcServiceOptions,
+  IpcResponse,
+} from "./decorators"
+
+export {
+  registry,
+  createPreloadApiByNamespace,
+  setupRegistryIpc,
+} from "./registry"
