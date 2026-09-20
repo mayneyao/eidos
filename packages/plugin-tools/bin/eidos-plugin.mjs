7cfa931b694a3485cb4c21a9ab95ac36fb508cf0
Mayne <i@gine.me>
2026-09-20 21:40:06 +0800

    chore(release): prepare plugin packages 0.1.4

--- packages/plugin-tools/bin/eidos-plugin.mjs
+++ packages/plugin-tools/bin/eidos-plugin.mjs
@@ -38,8 +38,8 @@ export async function create(directory) {
       "pack:plugin": "eidos-plugin pack .",
     },
     devDependencies: {
-      "@eidos.space/plugin-sdk": "^0.1.3",
-      "@eidos.space/plugin-tools": "^0.1.3",
+      "@eidos.space/plugin-sdk": "^0.1.4",
+      "@eidos.space/plugin-tools": "^0.1.4",
     },
   }
   const tsconfig = {
